728ebf75dfe1c081185283c89f63aef458121b6c
Siffster85 <ylsaxon@googlemail.com>
2024-06-07 10:01:53 +0100

    I forgot to do regular pushes, so here we have a completed thesaurus function.

--- thesaurus.py
+++ thesaurus.py
@@ -1,13 +1,28 @@
 import json
+import difflib
 
 data = json.load(open("data.json"))
 
 def lookup(word):
-    if word in data:
-        return data[word]
+    lower_word = word.lower()
+    if lower_word in data:
+        return data[lower_word]
     else:
-        return "This word doesn't exist within this dictionary. Please try again."
+        suggestion = difflib.get_close_matches(lower_word, data.keys(), n=1)
+        if len(suggestion) > 0:
+            response = input("This word doesn't exist within this dictionary, did you mean '" + suggestion[0] + "' instead? Enter 'y' if yes or 'n' if no: ")
+            if response == "y":
+                return data[suggestion[0]]
+            if response == 'n':
+                return "Please try again."
+            return "Your input is invalid, please try again."
+        else:
+            return "This word doesn't exist within this dictionary, please try again."
 
 word = input("What word would you like to lookup? ")
+output = lookup(word)
 
-print(lookup(word))
+if type(output) == list:
+    for item in output:
+        print(item)
+else: print (output)
